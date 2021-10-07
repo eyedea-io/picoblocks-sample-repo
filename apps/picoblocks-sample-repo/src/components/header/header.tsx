914221b36cf0fa88bfa7ea39b33530b3159ec2c1
Hubert Zub <hubert.zub@eyedea.io>
2021-10-07 09:25:30 +0200

    chore: extracted hamburger icon

--- apps/picoblocks-sample-repo/src/components/header/header.tsx
+++ apps/picoblocks-sample-repo/src/components/header/header.tsx
@@ -1,6 +1,7 @@
+import cx from 'classnames';
 import React from 'react';
 import { Logo } from '../logo/logo';
-import cx from 'classnames';
+import { ReactComponent as HamburgerIcon } from './hamburger.svg';
 
 export interface HeaderProps {
   items: {
@@ -31,22 +32,7 @@ export const Header: React.FC<HeaderProps> = ({ items }) => {
         })}
       </div>
       <button className="md:hidden bg-white rounded-md p-2 -mr-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-        <svg
-          className="h-6 w-6"
-          x-description="Heroicon name: outline/menu"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
+        <HamburgerIcon />
       </button>
     </div>
   );
